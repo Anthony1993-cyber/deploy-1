1e543e35887d8211e6f33fba16d973d3646115f6
Anthony Giordano <anthonygiordano1993@gmail.com>
2023-04-05 19:21:04 +0200

    Commit

---
+++ src/main/java/co/develhope/deploy1/controllers/BasicController.java
@@ -0,0 +1,22 @@
+package co.develhope.deploy1.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/name")
+public class BasicController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("")
+    public String getName(){
+        return environment.getProperty("myCustomEnvs.devName");
+    }
+
+
+}
